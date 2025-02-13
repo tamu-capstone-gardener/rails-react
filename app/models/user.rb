e4dcef5af5ab9457290161fabfeab0d4ca2be83b
caden-miller <cadenmiller40@gmail.com>
2025-02-12 18:40:58 -0600

    fixed a couple merge issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,4 @@
 class User < ApplicationRecord
-<<<<<<< Updated upstream
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :omniauthable, omniauth_providers: [ :google_oauth2 ]
-
-  def self.from_google(email:, full_name:, uid:, avatar_url:)
-    create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
-=======
   devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   validates :email, presence: true, uniqueness: true
@@ -25,6 +17,5 @@ class User < ApplicationRecord
     user.provider = auth.provider
     user.save!
     user
->>>>>>> Stashed changes
   end
 end
