d9ee0ceffd78af0fa6eff188fae3115961d809dd
Kevin Johnson <john2003.kjj@gmail.com>
2025-02-17 10:28:08 -0600

    added basic mqtt publishing code

--- app/controllers/mqtt_controller.rb
+++ app/controllers/mqtt_controller.rb
@@ -1,12 +1,27 @@
-require 'mqtt'
+require "mqtt"
 
 class MqttController < ApplicationController
+  # Needed for "public" APIs
+  skip_before_action :verify_authenticity_token
+
+  # TODO
+  # From server -> device
+  # Send signals to water, change schedule of watering
+  # From device <- server
+  # Low battery information, low water information, sensor data
+
   def publish_data
-    topic = "plant_hub/sensor_data"
     message = { sensor_id: params[:sensor_id], value: params[:value] }.to_json
-    MQTT::Client.connect('mqtt://your-mqtt-broker') do |client|
-      client.publish(topic, message)
+    secrets = Rails.application.credentials.hivemq
+    MQTT::Client.connect(
+      host: secrets[:url],
+      port: secrets[:port],
+      username: secrets[:username],
+      password: secrets[:password],
+      ssl: true
+    ) do |client|
+      client.publish(secrets[:topic], message)
     end
-    render json: { status: 'success', message: 'Data published' }
+    render json: { status: "success", message: "Data published" }
   end
 end
