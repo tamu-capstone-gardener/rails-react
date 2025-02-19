4f660905514be41aa261dda570297d3b3c8fbec1
Kevin Johnson <john2003.kjj@gmail.com>
2025-02-18 23:06:25 -0600

    fixed rubocop offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  devise :omniauthable, omniauth_providers: [:google_oauth2]
+  devise :omniauthable, omniauth_providers: [ :google_oauth2 ]
 
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
-  
+
   has_many :plant_modules, primary_key: :uid, foreign_key: :user_id, dependent: :destroy
 
   def self.from_omniauth(auth)
@@ -11,7 +11,7 @@ class User < ApplicationRecord
 
     user.uid = auth.uid
     user.email = auth.info.email
-    user.username ||= auth.info.email.split('@').first # Default username
+    user.username ||= auth.info.email.split("@").first # Default username
     user.full_name = auth.info.name
     user.avatar_url = auth.info.image
     user.provider = auth.provider
