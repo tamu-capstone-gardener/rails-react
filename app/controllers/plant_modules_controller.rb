7ba4506d3ff801c4b4d6bc8290d1cb8c30c927bd
caden-miller <cadenmiller40@gmail.com>
2025-02-19 10:40:20 -0600

    added time charts to the plant module view

--- app/controllers/plant_modules_controller.rb
+++ app/controllers/plant_modules_controller.rb
@@ -1,5 +1,6 @@
 class PlantModulesController < ApplicationController
   def show
     @plant_module = PlantModule.find(params[:id])
+    @sensors = @plant_module.sensors.includes(:time_series_data)
   end
 end
