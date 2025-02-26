32b7fca5292aeabbdab32cf022a81db09ffcde3a
Kevin Johnson <john2003.kjj@gmail.com>
2025-02-26 10:35:54 -0600

    ran rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,15 +8,15 @@ class ApplicationController < ActionController::Base
 
   def set_navbar_links
     @page_links = [
-      { name: 'Home', path: root_path },
-      { name: 'Modules', path: plant_modules_path },
+      { name: "Home", path: root_path },
+      { name: "Modules", path: plant_modules_path },
       # { name: 'Posts (TODO)', path: root_path },
       # { name: 'Advice (TODO)', path: root_path },
       # { name: 'Data (TODO)', path: root_path },
       # { name: 'Schedules (TODO)', path: root_path },
       # { name: 'Settings (TODO)', path: root_path },
       # { name: 'Profile (TODO)', path: root_path },
-      { name: 'Logout', path: destroy_user_session_path, method: :delete }
+      { name: "Logout", path: destroy_user_session_path, method: :delete }
     ]
   end
 end
