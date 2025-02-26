039dfcef32ec1cc7171c942414691efc6869c9ab
Kevin Johnson <john2003.kjj@gmail.com>
2025-02-26 09:47:40 -0600

    renamed simple create option, also added verification to plant_module show

--- app/controllers/plant_modules_controller.rb
+++ app/controllers/plant_modules_controller.rb
@@ -1,13 +1,18 @@
 class PlantModulesController < AuthenticatedApplicationController
   def show
-    @plant_module = PlantModule.find(params[:id])
+    @plant_module = PlantModule.find_by(id: params[:id])
+    if @plant_module.nil?
+      redirect_to plant_modules_path, alert: "Plant module not found."
+    elsif @plant_module.user != current_user
+      redirect_to plant_modules_path, alert: "You are not authorized to access this plant module."
+    end
   end
 
   def index
     @plant_modules = current_user.plant_modules
   end
 
-  def test_create
+  def simple_create
     PlantModule.create!(
       id: SecureRandom.uuid,
       user_id: current_user.uid,
