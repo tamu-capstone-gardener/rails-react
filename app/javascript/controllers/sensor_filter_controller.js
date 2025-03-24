bce18f9b3f954eb4a1736a5b541cc6889587d270
caden-miller <cadenmiller40@gmail.com>
2025-03-23 23:23:09 -0500

    fixed the sensor charts - need to make it so that the microcontroller only reads data once a minute or even slower so that the graphs will look better.

--- app/javascript/controllers/sensor_filter_controller.js
+++ app/javascript/controllers/sensor_filter_controller.js
@@ -1,18 +1,16 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["startDate", "endDate"];
+  static targets = ["startDate"];
   static values = { sensorId: String };
 
   update() {
     const startDate = this.startDateTarget.value;
-    const endDate = this.endDateTarget.value;
     const sensorId = this.sensorIdValue;
 
     // Build query parameters if dates are provided.
     const params = new URLSearchParams();
     if (startDate) params.append("start_date", startDate);
-    if (endDate) params.append("end_date", endDate);
 
     fetch(`/sensors/${sensorId}/time_series?${params.toString()}`, {
       headers: { Accept: "text/vnd.turbo-stream.html" }
