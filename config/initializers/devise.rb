a87c33de6592088e036fe7217e692f5c8e449395
Kevin Johnson <john2003.kjj@gmail.com>
2025-03-26 10:04:56 -0500

    deploying with kamal, neon db config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,8 +9,8 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.omniauth :google_oauth2, Rails.application.credentials.google[:oauth_client_id],
-                                    Rails.application.credentials.google[:oauth_client_secret]
+  config.omniauth :google_oauth2, Rails.application.credentials.dig(:google, :oauth_client_id),
+                                    Rails.application.credentials.dig(:google, :oauth_client_secret)
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
