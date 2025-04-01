8798934d93535d4aba1df59d5078bb95d6769fd7
caden-miller <cadenmiller40@gmail.com>
2025-04-01 02:57:08 -0500

    fixed some stuff with the filtering and recommendations

--- app/services/plant_recommendation_service.rb
+++ app/services/plant_recommendation_service.rb
@@ -1,4 +1,5 @@
 class PlantRecommendationService
+  include ZipCodeHelper
   def initialize(location_type:, zip_code: nil, filters: {})
     @location_type = location_type
     @zip_code = zip_code
@@ -39,29 +40,30 @@ class PlantRecommendationService
 
 
   def recommend_outdoor
-    return Plant.none if @zip_code.blank?
+    max_height = (@filters[:max_height].presence || 10.0).to_f
+    max_width  = (@filters[:max_width].presence || 10.0).to_f
+    edibility_rating = (@filters[:edibility_rating].presence || "3").to_i
+
+    return Plant.all.page(@filters[:page]).per(10) if @zip_code == "" && max_height == 10.0 && max_width == 10.0 && edibility_rating == 3
+    return Plant.where("height < ? AND width < ?", max_height, max_width)
+                .where("CAST(edibility AS int) >= ?", edibility_rating)
+                .page(@filters[:page]).per(10) if @zip_code == ""
 
     zone_data = zone_for_zip(@zip_code)
     return Plant.none unless zone_data
 
     zone_num = zone_data[:zone].match(/\d+/)[0]
 
-    max_height = (@filters[:max_height].presence || 10.0).to_f
-    max_width  = (@filters[:max_width].presence || 10.0).to_f
-    edibility_rating = (@filters[:edibility_rating].presence || "3").to_i
-
     if max_height == 10.0 and max_width == 10.0 and edibility_rating == 3
-      Plant
-        .where("hardiness_zones ILIKE ?", "%#{zone_num}%")
-        .page(@filters[:page])
-        .per(10)
+      Plant.where("hardiness_zones ILIKE ?", "%#{zone_num}%")
+           .page(@filters[:page])
+           .per(10)
     else
-      Plant
-        .where("hardiness_zones ILIKE ?", "%#{zone_num}%")
-        .where("height < ? AND width < ?", max_height, max_width)
-        .where("CAST(edibility AS int) >= ?", edibility_rating)
-        .page(@filters[:page])
-        .per(10)
+      Plant.where("hardiness_zones ILIKE ?", "%#{zone_num}%")
+           .where("height < ? AND width < ?", max_height, max_width)
+           .where("CAST(edibility AS int) >= ?", edibility_rating)
+           .page(@filters[:page])
+           .per(10)
     end
   end
 end
